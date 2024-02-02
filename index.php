418faa8f3828fdfa7a49e88b4ab72bf96d99b03d
Jonte88 <jonathan.thj15@gmail.com>
2024-02-02 10:37:40 +0100

    Adding simple login and dashboard without database 	modified:   index.php 	new file:   other/dashboard.php 	new file:   other/logout.php

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-<?php 
-    
+<?php
+
+    session_start();
+
+    if (isset($_POST["submit"])) {
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $password = $_POST["password"];
+
+        if ($username == "Jonathan" && $password == "123") {
+            $_SESSION["username"] = $username;
+            header("Location: /other/dashboard.php");
+        } else {
+            echo "Incorrect Username or Password!";
+        }
+    }
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+    <div>
+        <label for="username">Username: </label>
+        <input type="text" name="username">
+    </div>
+    <div>
+        <label for="password">Password: </label>
+        <input type="password" name="password">
+    </div>
+    <input type="submit" value="Submit" name="submit">
+</form>
