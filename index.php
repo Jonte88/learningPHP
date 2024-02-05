e5bc9e7274e312ad447e4f9f1ba992d1054d8187
Jonte88 <jonathan.thj15@gmail.com>
2024-02-05 08:34:29 +0100

    I dont know 	new file:   css/style.css 	modified:   index.php 	new file:   tailwind.config.js

--- index.php
+++ index.php
@@ -15,15 +15,34 @@
         }
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Thing</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
 
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-    <div>
-        <label for="username">Username: </label>
-        <input type="text" name="username">
+    <div class="flex justify-center items-center h-screen">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="bg-white py-[2%] px-[2%] rounded-2xl w-1/4">
+            <h1 class="text-7xl	text-center font-bold">Log in</h1>
+            <div class="mt-[10%]">
+                <label for="username" class="text-center font-bold ml-2">Username: </label><br>
+                <input type="text" name="username" class="border-2 border-black rounded-xl w-full p-[1%]">
+            </div>
+            <div class="mt-[3%]">
+                <label for="password" class="text-center font-bold ml-2">Password: </label><br>
+                <input type="password" name="password" class="border-2 border-black rounded-xl w-full p-[1%]">
+            </div>
+            <input type="submit" value="Submit" name="submit" class="bg-black text-white rounded-xl px-[3%] py-[1.5%] mt-[5%]">
+        </form>
     </div>
-    <div>
-        <label for="password">Password: </label>
-        <input type="password" name="password">
-    </div>
-    <input type="submit" value="Submit" name="submit">
-</form>
+    <footer class="text-white absolute inset-x-0 bottom-0 h-16 font-bold m-auto">
+        <p class="text-center text-xl">&copy; Jonathan Th-J 2024</p>
+    </footer>
+
+</body>
+</html>
